e3b1e23664e8c2c813f5e480410d1b994c3df51a
Moe Aye Aye Myint <moeayeayemyint@Moes-MacBook-Pro.local>
2026-07-27 11:39:00 +0700

    Connect database to Neon PostgreSQL

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ import os
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
 if DATABASE_URL:
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 else:
